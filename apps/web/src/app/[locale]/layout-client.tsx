ab50025ae33999dde3338c08518b1c268554bf51
kirianL <kirianluna.u@gmail.com>
2026-01-17 23:31:20 -0600

    fix: add missing React import in layout-client.tsx

--- apps/web/src/app/[locale]/layout-client.tsx
+++ apps/web/src/app/[locale]/layout-client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { ScrollRestoration } from "@/components/scroll-restoration";
